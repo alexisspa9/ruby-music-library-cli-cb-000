c3d6b080d6991dd2e4de27a3a7fd4f1f40001c01
Alexander Spanos <alexisspa9@gmail.com>
2018-03-26 19:07:10 +0000

    automatically backed up by learn

--- lib/001_song_basics.rb
+++ lib/001_song_basics.rb
@@ -44,6 +44,7 @@ class Song
      all.detect{ |s| s.name == name }
   end
   def self.find_or_create_by_name(name)
+    self.find_by_name(name) ? self.find_by_name(name) : self.create(name)
 
   end
 
