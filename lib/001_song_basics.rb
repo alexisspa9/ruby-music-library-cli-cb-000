5044ac5fabce8abe0868fa44cd20f016b0ef249a
Alexander Spanos <alexisspa9@gmail.com>
2018-03-26 18:51:04 +0000

    automatically backed up by learn

--- lib/001_song_basics.rb
+++ lib/001_song_basics.rb
@@ -1,10 +1,11 @@
 class Song
-  attr_accessor :name, :artist
+  attr_accessor :name, :artist, \:genre
   @@all = []
 
   def initialize(name, artist = nil, genre = nil)
     @name = name
   self.artist = artist if artist
+  self.genre = genre if genre
   end
   def artist=(artist)
       @artist = artist
