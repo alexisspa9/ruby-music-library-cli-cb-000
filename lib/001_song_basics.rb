285e4d5afa14cbe7ec3c316f7274b789d936a046
Alexander Spanos <alexisspa9@gmail.com>
2018-03-26 18:52:44 +0000

    automatically backed up by learn

--- lib/001_song_basics.rb
+++ lib/001_song_basics.rb
@@ -13,7 +13,7 @@ class Song
     end
     def genre=(genre)
         @genre = genre
-genre.songs << self unless genre.songs.include?(self)
+        genre.songs << self unless genre.songs.include?(self)
       end
   def self.all
     @@all
